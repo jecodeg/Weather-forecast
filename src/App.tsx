841340d44dae6d9add57c1deafef61c7c422230a
jecodeg <j.ganesh1127@gmail.com>
2024-07-05 10:02:10 +0530

    Implementation completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Forecast from './components/Forecast';
+import Search from './components/Search';
+import useForecast from './hooks/useForecast';
 
-function App() {
+const App = (): JSX.Element => {
+  const { forecast, options, term, onOptionSelect, onSubmit, onInputChange } = useForecast();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="flex justify-center items-center bg-gradient-to-br from-purple-400 via-sky-400 to-red-400 min-h-screen w-full">
+      {forecast ? (
+        
+        <Forecast data={forecast} />
+        
+      ) : (
+        <Search
+          term={term}
+          options={options}
+          onInputChange={onInputChange}
+          onOptionSelect={onOptionSelect}
+          onSubmit={onSubmit}
+        />
+      )}
+    </main>
   );
-}
+};
 
 export default App;
